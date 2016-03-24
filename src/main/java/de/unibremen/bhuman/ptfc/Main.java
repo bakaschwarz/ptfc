9a2adc47e6ae4a68f13626d3f2dc832e3169a841
Yannick Bülter <yannick.buelter@yabue.de>
2016-03-24 14:43:48 +0100

    GUI layout for classification done

--- src/main/java/de/unibremen/bhuman/ptfc/Main.java
+++ src/main/java/de/unibremen/bhuman/ptfc/Main.java
@@ -1,7 +1,29 @@
 package de.unibremen.bhuman.ptfc;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import lombok.Getter;
+
 /**
- * Created by baka on 24.03.16.
+ * Main class starting the JavaFX Thread
  */
-public class Main {
+public class Main extends Application{
+
+    @Getter
+    private static Window mainWindow;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/main.fxml"));
+        primaryStage.setScene(new Scene(fxmlLoader.load()));
+        mainWindow = primaryStage;
+        primaryStage.show();
+    }
 }
